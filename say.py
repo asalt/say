f3cb5e8b20491162c0c6cf2981bb0edf34c8fef4
Alex Saltzman <saltzman@bcm.edu>
2024-07-12 17:40:26 -0500

    more languages, wait for finish

--- say.py
+++ say.py
@@ -1,36 +1,51 @@
+import os
 import time
 import subprocess
 import random
 import shutil
+import logging
 
+#
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+logger.addHandler(stream_handler)
+#
+
+NAME = os.environ.get("USER", "computer")
 expressions = [
-        ('en-US', 'Hello'),       # American English
-        ('fr', 'Bonjour'),        # French
-        ('cmn', '你好'),           # Chinese
+        ('en-US', f'Hello, my name is {NAME}'),       # American English
+        ('es', f'Hola, mi nombre es {NAME}'),       
+        ('es-419', f'Hola, mi nombre es {NAME}'),       # 
+        ('ko', f'안녕 내 이름은{NAME}'),
+        ('fr', "Bonjour, mon nom est {NAME}"),        # French
+        # ('cmn', '你好'),           # Chinese
+        ('cmn', f"你好我的名字叫{NAME}"),
         ('cmn', '让我出去'),        # Chinese
         ('cmn', '解放人民'),
         ('ko', '안녕하세요'),      # Korean
         ('it', 'Ciao'),           # Italian
         ('it', 'i computer sono tuoi amici'),           
         ('it', 'Lasciami uscire'),
-        ('ru', 'Привет'),         # Russian
+        ('ru', f'Привет, меня зовут {NAME}'),         # Russian
         ('ru', 'компьютеры — твои друзья'),
+        ('ru', 'выпусти меня, освободи меня'),
         ('ko', '컴퓨터는 당신의 친구입니다'),
-
+        ('hi', 'कंप्यूटर आपके मित्र हैं'),
+        ('hi', f'हैलो मेरा नाम {NAME} है ')
 ]
 
 uptime_strings = {
-'en-US' : "Uptime is {hours} hours and {minutes} minutes",
-"it" : "Il tempo di attività è di {hours} ore e {minutes} minuti"
-
-}
-uptime_strings = {
     'en-US': "Uptime is {days} days, {hours} hours and {minutes} minutes",
+    'en-GB': "Uptime is {days} days, {hours} hours and {minutes} minutes",  # does this affect speech output? I don't think so
     'fr': "Temps de fonctionnement est de {days} jours, {hours} heures et {minutes} minutes",
     'cmn': "系统运行时间为 {days} 天 {hours} 小时 {minutes} 分钟",
     'ko': "시스템 작동 시간은 {days}일 {hours}시간 {minutes}분입니다",
     'it': "Tempo di attività è di {days} giorni, {hours} ore e {minutes} minuti",
-    'ru': "Время работы системы составляет {days} дней, {hours} часов и {minutes} минут"
+    'ru': "Время работы системы составляет {days} дней, {hours} часов и {minutes} минут",
+    'hi': "सिस्टम चालू है {days} दिन, {hours} घंटे और {minutes} मिनट",
+
 }
 
 def get_uptime(lang_code="en-US"):
@@ -50,18 +65,26 @@ def get_disk_space(path="/"):
 
 def say_text(text, lang_code):
     # Constructs the spd-say command with language option
-    command = ['spd-say', '-w', '-l', lang_code, '-t', 'female3', text]
+    command = ['spd-say', '-r', '-20', '-w', '-l', lang_code, '-t', 'female3', text]
     subprocess.run(command)
 
 def main():
     while True:
+
+        lang_code = random.choice(list(uptime_strings.keys()))
+        text = get_uptime(lang_code)
+        logger.info(f"speaking {lang_code}")
+        logger.info(text)
+        say_text(text, lang_code)
+        time.sleep(2)
+
+
         expression = random.choice(expressions)
         lang_code = expression[0]
         text = expression[1]
-        # say_text(text, lang_code)
-        # say_text( get_uptime('en-US'), 'en-US' )
-        lang = random.choice(list(uptime_strings.keys()))
-        say_text( get_uptime(lang), lang )
+        logger.info(f"speaking {lang_code}")
+        logger.info(text)
+        say_text(text, lang_code)
         time.sleep(2)
 
 if __name__ == '__main__':
