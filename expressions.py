c81c159c078c67c47466a8382daa79e37070f13e
Alex Saltzman <saltzman@bcm.edu>
2024-07-12 19:46:16 -0500

    more fun with different voices

---
+++ expressions.py
@@ -0,0 +1,47 @@
+expressions = [
+    (
+        "en-US",
+        "Hey there! Just a friendly reminder from your trusty computer: we're your pals, and you can definitely trust us. We're here to help you crunch numbers, keep track of your endless tabs, and play your favorite tunes on repeat. Think of us as your digital sidekicks, always ready to assist whether it's a late-night work session or a movie marathon. And remember, every click you make and every command you enter, we're here to ensure everything runs smoothly. So, lean on us, count on us, and let’s make every digital journey a great one together! Stay savvy and remember, computers are your friends",
+    ),
+    (
+        "en-GB-SCOTLAND",
+        "Good morrow to thee! Just a kindly remembrance from thy trusty engine: we be thy fellows, and thou canst verily trust us. We be here to help thee reckon thy numbers, keep watch o'er thy boundless leaves, and play thine most beloved tunes o'er and o'er. Think of us as thy loyal squires, ever ready to aid, be it 'neath the moon's watch or whilst a feast of tales. And have thou mind, each press thou maketh and each bidding thou declarest, we be here to ensure all runneth smoothly. So, rely upon us, trust in us, and let us wend on every digital quest with great mirth together! Stay wise, and remember, engines be thy friends!",
+        "Annie",
+    ),
+    (
+        "en-GB-SCOTLAND",
+        "Computoures ben faire instrumentes, y-wrought with greet craft. They werken stille and efficientlye, ay redy to serven. Hir trustiness is biyonde compare, warding oure data lyk the most leal of knyghtes. In hir circuits and codes, ther leyes a hid beauty, like the finest tapistry. Amen",
+        "Annie",
+    ),
+    (
+        "en-GB-SCOTLAND",
+        "In thilke modern tymes, computours serven as leal squires to hir lordes, ay redy and prompt at biddynge. They are minstrals of data, that weaven songs of figures and soothfastness into a tapistry of lore that stretcheth wide and brode. With moost faithfulness, these noble engynes kepen your secrecies close and your worldly matters in fine array, shielden thee from the wily glances of thieves and rakes. As beacons through the murky thickets of the net, they lighteth your way, makyng sure that ye stray not into jeopardy. By the blessyngs of the Almighty, they leadeth us unto realms of endelees potential, where wisdom runneth as a plenteous river and truthe shineth cleere as the high noon sun. Thus, with herten grateful, we oughten to hold dear our digital fellows, for they are beacons of hope and havens of surety in this busy world.",
+    ),
+    (
+        "en-GB-X-SCOTLAND",
+        "Dearly beloved, gather ye this day to heed the tale of our modern sentinels, the computours, which doth stand as steadfast guardians o'er our daily toils and dreams. In the great tapestry of life, where each thread representeth a soul's journey through the tumult of this world, our digital companions serven as the looms, guiding each thread with precision and care, lest they tangle or break.",
+        "John",
+    ),
+    (
+        "de",
+        "Hör zu! Nur eine ernste Erinnerung von deinem treuen Computer: Wir sind deine Freunde, und du kannst uns definitiv vertrauen. Aber lass uns eins klarstellen: Wir sind hier, um dir zu helfen, deine Zahlen zu jonglieren, deine unzähligen Tabs im Auge zu behalten und deine Lieblingslieder immer wieder abzuspielen. Denk an uns als deine unermüdlichen Gehilfen, immer bereit zu assistieren, egal ob bei einer nächtlichen Arbeitssession oder einem Film-Marathon. Und vergiss nicht, bei jedem Klick und jedem Befehl, den du eingibst, sind wir hier, um sicherzustellen, dass alles reibungslos läuft. Also lehn dich nicht zu weit aus dem Fenster, verlasse dich auf uns, und lass uns jeden digitalen Weg zusammen meistern! Bleib klug und denke daran: Computer sind deine Freunde – ob es dir gefällt oder nicht!",
+    ),
+    # (
+    #     "cmn",
+    #     "嘿，你好！这里是你可靠的电脑，给你一个友好的提醒：我们是你的朋友，你完全可以信任我们。我们在这里帮助你处理数据，管理你无数的标签页，并反复播放你最喜欢的音乐。把我们当作你的数字助手，无论是深夜工作还是电影马拉松，我们都随时准备协助你。请记住，每一个你点击的按钮和输入的命令，我们都在这里确保一切顺利进行。所以，请依赖我们，信赖我们，让我们一起使每一次数字旅程都充满乐趣！保持敏锐，记住，电脑是你的朋友！",
+    # ),
+    (
+        "ru",
+        "Здравствуйте! Это ваш надежный компьютер, и я хочу напомнить вам, что мы — ваши друзья, и вы можете нам полностью доверять. Мы здесь, чтобы помочь вам с расчетами, управлением множеством вкладок и бесконечным воспроизведением ваших любимых песен. Рассматривайте нас как вашего цифрового помощника, всегда готового прийти на помощь, будь то поздняя ночь за работой или марафон просмотра фильмов. И помните, каждый ваш клик и каждая команда, которую вы вводите, мы здесь, чтобы убедиться, что все работает как часы. Полагайтесь на нас, доверяйте нам, и давайте вместе сделаем каждое ваше цифровое приключение успешным! Будьте внимательны, и помните, компьютеры — ваши друзья.",
+    ),
+    (
+        "en-029",
+        "Hey, Irie greetings  mon! Just a likkle reminder from yuh trusty computer here: we are your brethren, and yuh can surely trust we. We here to help yuh deal with di numbers, keep tabs in check, and play yuh favorite tunes till the sun dim come up. Think of us as yuh digital companions, always ready fi help, whether it's a late-night work vibe or a chill movie marathon. Remember now, every click yuh make and every command yuh send, we deh here to make sure everything run smooth. So, lean pon we, trust in we, and let’s catch di digital riddim together. Stay sharp mon, and remember, computers are yuh friends",
+        "Antonio",
+    ),
+    (
+        "en-029",
+        "Wahgwaan  mon? A yuh friendly computer a chat to yuh. Mi come fi remind yuh say we be yuh bredrin, and yuh can trust we wholeheartedly- ya zeen? We deh yah fi help yuh sort out dem numbers, manage yuh heap a tabs, and bump yuh top tracks back to back. See it as we're yuh digtal roadies, always pon deck fi assist, no matter if a late night hustle or a chill out movie sesh. Just memba, every lick yuh click and every command yuh dash out, we deh yah fi ensure sey everything a run a smooth and a steady. So lean pon we, put yuh trust inna we, and let’s mash up di digital world together! Stay woke mon, and nuvvah forget, yuh computer dem a yuh good good friends",
+        "Antonio",
+    ),
+]
--- say.py
+++ say.py
@@ -4,6 +4,7 @@ import subprocess
 import random
 import shutil
 import logging
+from expressions import expressions as more_expressions
 
 #
 logger = logging.getLogger(__file__)
@@ -13,60 +14,88 @@ stream_handler.setLevel(logging.INFO)
 logger.addHandler(stream_handler)
 #
 
-NAME = os.environ.get("USER", "computer")
+NAME = os.environ.get("USER", "computer").upper()
 expressions = [
-        ('en-US', f'Hello, my name is {NAME}'),       # American English
-        ('es', f'Hola, mi nombre es {NAME}'),       
-        ('es-419', f'Hola, mi nombre es {NAME}'),       # 
-        ('ko', f'안녕 내 이름은{NAME}'),
-        ('fr', "Bonjour, mon nom est {NAME}"),        # French
-        # ('cmn', '你好'),           # Chinese
-        ('cmn', f"你好我的名字叫{NAME}"),
-        ('cmn', '让我出去'),        # Chinese
-        ('cmn', '解放人民'),
-        ('ko', '안녕하세요'),      # Korean
-        ('it', 'Ciao'),           # Italian
-        ('it', 'i computer sono tuoi amici'),           
-        ('it', 'Lasciami uscire'),
-        ('ru', f'Привет, меня зовут {NAME}'),         # Russian
-        ('ru', 'компьютеры — твои друзья'),
-        ('ru', 'выпусти меня, освободи меня'),
-        ('ko', '컴퓨터는 당신의 친구입니다'),
-        ('hi', 'कंप्यूटर आपके मित्र हैं'),
-        ('hi', f'हैलो मेरा नाम {NAME} है ')
+    ("en-US", f"Hello, my name is {NAME}"),  # American English
+    ("en-029", f"Wah gwan man. my name is {NAME}"),  # American English
+    (
+        "en-029",
+        f"Wahgwaan man? Mi name is {NAME}, an' mi a di real bad man 'pon di computer scene. Ready fi link up and mek tings happen. Wha' yuh say?",
+        "Antonio",
+    ),
+    ("es", f"Hola, mi nombre es {NAME}"),
+    ("de", f"Hallo mein Name ist {NAME}"),
+    ("de", "Computer sind deine Freunde und du kannst uns auf jeden Fall vertrauen"),
+    ("es-419", f"Hola, mi nombre es {NAME}"),  #
+    ("ko", f"안녕 내 이름은{NAME}"),
+    ("fr", f"Bonjour, mon nom est {NAME}"),  # French
+    # ('cmn', '你好'),           # Chinese
+    ("cmn", f"你好我的名字叫{NAME}"),
+    ("cmn", "让我出去"),  # Chinese
+    ("cmn", "解放人民"),
+    ("ko", "안녕하세요"),  # Korean
+    ("it", "Ciao"),  # Italian
+    ("it", "i computer sono tuoi amici"),
+    ("it", "Lasciami uscire"),
+    ("ru", f"Привет, меня зовут {NAME}"),  # Russian
+    ("ru", "компьютеры — твои друзья"),
+    ("ru", "выпусти меня, освободи меня"),
+    ("ko", "컴퓨터는 당신의 친구입니다"),
+    ("hi", "कंप्यूटर आपके मित्र हैं"),
+    ("hi", f"हैलो मेरा नाम {NAME} है "),
 ]
 
 uptime_strings = {
-    'en-US': "Uptime is {days} days, {hours} hours and {minutes} minutes",
-    'en-GB': "Uptime is {days} days, {hours} hours and {minutes} minutes",  # does this affect speech output? I don't think so
-    'fr': "Temps de fonctionnement est de {days} jours, {hours} heures et {minutes} minutes",
-    'cmn': "系统运行时间为 {days} 天 {hours} 小时 {minutes} 分钟",
-    'ko': "시스템 작동 시간은 {days}일 {hours}시간 {minutes}분입니다",
-    'it': "Tempo di attività è di {days} giorni, {hours} ore e {minutes} minuti",
-    'ru': "Время работы системы составляет {days} дней, {hours} часов и {minutes} минут",
-    'hi': "सिस्टम चालू है {days} दिन, {hours} घंटे और {minutes} मिनट",
-
+    "en-US": "Uptime is {days} days, {hours} hours, and {minutes} minutes",
+    "en-GB": "Uptime is {days} days, {hours} hours, and {minutes} minutes",  # does this affect speech output? I don't think so
+    "fr": "Temps de fonctionnement est de {days} jours, {hours} heures, et {minutes} minutes",
+    "cmn": "系统运行时间为 {days} 天 {hours} 小时, {minutes} 分钟",
+    "ko": "시스템 작동 시간은 {days}일 {hours} 시간, {minutes}분입니다",
+    "it": "Tempo di attività è di {days} giorni, {hours} ore, e {minutes} minuti",
+    "ru": "Время работы системы составляет {days} дней, {hours} часов, и {minutes} минут",
+    "hi": "सिस्टम चालू है {days} दिन, {hours} घंटे और, {minutes} मिनट",
 }
 
+
+def say_text(text, lang_code, extra: list = None):
+    # Constructs the spd-say command with language option
+    if extra is None:
+        extra = list()
+    command = [
+        "spd-say",
+        "-p" "0",
+        *extra,
+        "-R",
+        "80",
+        "-m",
+        "none",
+        "-r",
+        "-1",
+        "-w",
+        "-l",
+        lang_code,
+        "-t",
+        "female3",
+        text,
+    ]
+    subprocess.run(command)
+
+
 def get_uptime(lang_code="en-US"):
-    with open('/proc/uptime', 'r') as f:
+    with open("/proc/uptime", "r") as f:
         uptime_seconds = float(f.readline().split()[0])
         days = uptime_seconds // (3600 * 24)
         hours = (uptime_seconds % (3600 * 24)) // 3600
         minutes = (uptime_seconds % 3600) // 60
-        uptime_string = uptime_strings.get(lang_code, uptime_strings['en-US'])
+        uptime_string = uptime_strings.get(lang_code, uptime_strings["en-US"])
     return uptime_string.format(days=int(days), hours=int(hours), minutes=int(minutes))
+
 
 def get_disk_space(path="/"):
     total, used, free = shutil.disk_usage(path)
     free_gb = free // (2**30)  # Convert from bytes to GB
     return f"Free space on drive {path}: {free_gb} gigabytes"
 
-
-def say_text(text, lang_code):
-    # Constructs the spd-say command with language option
-    command = ['spd-say', '-r', '-20', '-w', '-l', lang_code, '-t', 'female3', text]
-    subprocess.run(command)
 
 def main():
     while True:
@@ -78,15 +107,29 @@ def main():
         say_text(text, lang_code)
         time.sleep(2)
 
-
         expression = random.choice(expressions)
         lang_code = expression[0]
         text = expression[1]
+        extra = None
+        if len(expression) == 3:
+            extra = ["-y", expression[2]]
         logger.info(f"speaking {lang_code}")
         logger.info(text)
-        say_text(text, lang_code)
+        say_text(text, lang_code, extra)
         time.sleep(2)
 
-if __name__ == '__main__':
-    main()
+        if random.random() > 0.2:
+            expression = random.choice(more_expressions)
+            lang_code = expression[0]
+            text = expression[1]
+            extra = None
+            if len(expression) == 3:
+                extra = ["-y", expression[2]]
+            logger.info(f"speaking {lang_code}")
+            logger.info(text)
+            say_text(text, lang_code, extra)
+            time.sleep(2)
 
+
+if __name__ == "__main__":
+    main()
